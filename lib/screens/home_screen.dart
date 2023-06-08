292419447ac52cfec3def8a5141a2c90399d5fca
Ahmad Ershad Omari <ahmad.ershad.2014@gmail.com>
2023-06-08 16:12:49 +0430

    add bottom navigation bar to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,42 +13,55 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 0.0,
-        backgroundColor: Colors.white,
-        toolbarHeight: 80.0,
-        title: Row(
-          children: [
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          toolbarHeight: 80.0,
+          title: Row(children: [
             Icon(
               Icons.location_on,
               color: Colors.blue.shade600,
+            ),
+            Text(
+              "Herat, Afghanistan",
+              style: TextStyle(
+                color: Colors.black,
               ),
-              Text(
-                "Herat, Afghanistan",
-                 style: TextStyle(
-                  color: Colors.black,
-                 ),
-                 ),
-            ]),
-            actions: [
-              IconButton(onPressed: () {
-
-            }, icon: Icon(
-              Icons.notifications,
-               color: Colors.green.shade600,
-               ),
-               ),
-          ]
-      ),
+            ),
+          ]),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.notifications,
+                color: Colors.green.shade600,
+              ),
+            ),
+          ]),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            children: [
-              
-            ],
-          ),  
+            children: [],
+          ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey.shade600,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.search), label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.heart), label: "Favorites"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.message_outlined), label: "Messages"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.person), label: "Profile"),
+        ],
       ),
     );
   }
